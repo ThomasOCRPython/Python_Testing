c0c2f7ce52a9a5a9f1174c58f8ad4a8bb5a88359
Thomas2020 <thomas.duval2020@campus-eni.fr>
2022-03-01 12:21:20 +0100

    commit wrongemail avec response 200

--- server.py
+++ server.py
@@ -26,8 +26,12 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    try:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+        return render_template('welcome.html',club=club,competitions=competitions)
+    except IndexError:
+        flash("Sorry, that email wasn't found.")
+        return render_template('index.html',error_message="Sorry, that email wasn't found.")
 
 
 @app.route('/book/<competition>/<club>')
